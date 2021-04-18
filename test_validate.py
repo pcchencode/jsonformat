7114d6b71840417bdbbff9f4db2d2702fbb6ddf5
pcchencode <zxp791228@gmail.com>
2021-04-18 22:10:55 +0800

    update

--- test_validate.py
+++ test_validate.py
@@ -2,7 +2,44 @@ import os
 import pytest
 from json_schema_validation import validate
 
+def test_for_normal():
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == True
+
 def test_for_null():
+    data = {"ad_network": ""
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
+    data = {"ad_network": "FOO"
+        , "date": ""
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": ""
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
     data = {"ad_network": "FOO"
         , "date": "2019-06-05"
         , "app_name": "LINETV"
@@ -12,14 +49,95 @@ def test_for_null():
         , "imp": 23}
     assert validate(data) == False
 
-def test_for_date():
+    data = {"ad_network": ""
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": None
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
     data = {"ad_network": "FOO"
-        , "date": "20190605"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": None
+        , "imp": 23}
+    assert validate(data) == False
+
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": None}
+    assert validate(data) == False
+
+def test_for_col_type():
+    data = {"ad_network": 123
+        , "date": "2019-06-05"
         , "app_name": "LINETV"
         , "unit_id": 55665201314
         , "request": 100
         , "revenue": 0.00365325
         , "imp": 23}
+    assert validate(data) == False
+
+    data = {"ad_network": "FOO"
+        , "date": 123
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": 123
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": "55665201314"
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": "100"
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
+    data = {"ad_network": "FOO"
+        , "date": "20190605"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": "0.00365325"
+        , "imp": 23}
+    assert validate(data) == False
+
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": "23"}
     assert validate(data) == False
 
 
@@ -31,3 +149,15 @@ def test_for_col_miss():
         , "revenue": 0.00365325
         , "imp": 23}
     assert validate(data) == False
+
+def test_for_col_redundant():
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23
+        , "redundant":"value"}
+    assert validate(data) == False
+

ec1cc1b1e582acad6c75a888645640349ccc0c1f
pcchencode <zxp791228@gmail.com>
2021-04-19 18:36:23 +0800

    update test for colomn type

--- test_for_column_type.py
+++ test_for_column_type.py
@@ -44,7 +44,18 @@ def test_date():
     assert validate(data) == False
 
 def test_app_name():
-    #app_name not int
+    #app_name not string
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": 123
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
+def test_unit_id():
+    #unit_id not int: string
     data = {"ad_network": "FOO"
         , "date": "2019-06-05"
         , "app_name": "LINETV"
@@ -54,14 +65,79 @@ def test_app_name():
         , "imp": 23}
     assert validate(data) == False
 
+    #unit_id not int: float
     data = {"ad_network": "FOO"
         , "date": "2019-06-05"
         , "app_name": "LINETV"
-        , "unit_id": 55665201314.1
+        , "unit_id": 55665201314.0
         , "request": 100
         , "revenue": 0.00365325
         , "imp": 23}
     assert validate(data) == False
+
+def test_request():
+    #request not int:string
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": "100"
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
+    #request not int:float
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100.0
+        , "revenue": 0.00365325
+        , "imp": 23}
+    assert validate(data) == False
+
+def test_revenue():
+    #revenue not Number:string
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": "0.00365325"
+        , "imp": 23}
+    assert validate(data) == False
+
+    #revenue is Number:int
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 123
+        , "imp": 23}
+    assert validate(data) == True
+
+def test_imp():
+    #imp not int:string
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": "23"}
+    assert validate(data) == False
+
+    #imp not int:float
+    data = {"ad_network": "FOO"
+        , "date": "2019-06-05"
+        , "app_name": "LINETV"
+        , "unit_id": 55665201314
+        , "request": 100
+        , "revenue": 0.00365325
+        , "imp": 23.0}
+    assert validate(data) == False
+
 
 
 
